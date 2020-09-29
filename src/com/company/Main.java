4da25e4fb7a78ecbac5c6ef35e430d8575c2d851
professorqtaku <professorqtaku@gmail.com>
2020-09-29 13:52:16 +0200

    added new class and tested new skyscraper

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,23 @@ public class Main {
 	    Skyscraper mannen = new Skyscraper.SkyscraperBuilder(100).
             name("Mannen")
             .build();
+	    /*
 	    
         System.out.println(mannen.toRealFloor(15));
         System.out.println(SolutionFromLecture.toRealFloor(15));
         for(var floor = 1; floor <= 220; floor++){
             var fakeFloor = SolutionFromLecture.toFakeFloor(floor);
             System.out.printf("floor: %d fakefloor: %d, back to real: %d\n", floor, fakeFloor, SolutionFromLecture.toRealFloor(fakeFloor)==floor ? 1: 0);
+        }
+        */
+
+        var skyScarper  = new SkyscraperMemorized(220);
+        for(var floor = 1; floor <=220; floor++) {
+            System.out.print("Floor: " + floor);
+            var fakeFloor = skyScarper.toFakeFloor(floor);
+            System.out.print(" fake floor: " + fakeFloor);
+            var realFloor = skyScarper.toRealFloor(fakeFloor);
+            System.out.println(" real floor: " + realFloor);
         }
     }
 }
